62bcbd6e5e320d9c2792da7fc1609a2610866c0b
ganesan g <ganeshghy0@gmail.com>
2023-05-14 18:51:43 +0530

    implement inheritan concept

--- vehicle.java
+++ vehicle.java
@@ -1,16 +1,40 @@
 public class vehicle {
+    int tyre = 6,head_light=2;
+    int max_gear = 8;
+    public void brake(){
+        System.out.println("This vehicle has brake");
+    }
+    public void clutch(){
+        System.out.println("This vehicle has clutch");
+    }
     public void accelerator(){
-        System.out.println("This vehicle have accelerator");
+        System.out.println("This vehicle has accelerator");
+    }
+    public void gear(){
+        System.out.println("This vehicle has maximum gear"+max_gear);
     }
 }
 class car extends vehicle{
-    public void accelerator(){
-        System.out.println("All cars has accelerator");
+
+    int tyre = 4,max_gear=6;
+    public void gear(){
+        System.out.println("car has maximum gear"+max_gear);
     }
+}
+class BMW extends car{
+    String color = "red",model_no = "212UY",car_name = "vivo";
+
 }
 class Main{
     public static void main(String args[]){
-        car v1 = new car();
-        v1.accelerator();
+        car c = new car();
+        BMW b1 = new BMW();
+        System.out.println("BMW car color"+b1.color);
+        System.out.println("BMW model no"+b1.model_no);
+        System.out.println("BMW car name"+b1.car_name);
+        b1.accelerator();
+        b1.brake();
+        b1.clutch();
+        b1.gear();
     }
 }
